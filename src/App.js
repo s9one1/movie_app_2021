64eb14d3ad619806b456dcaf44418a38997682d6
s9one1 <s9one1@gmail.com>
2021-09-15 10:30:52 +0900

    food컴포넌트에 Props전달

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
   return (
     <div>
       <h1>Hello React!!!!!!!!!!!!</h1>
-      <Food fav = "Kimchi" />
+      <Food fav = "Kimchi" something={true} papapa={['hello', 1, 2, 3, 4, true]} />
     </div>
   );
 }
